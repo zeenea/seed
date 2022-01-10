e8aa996746c79b57728d9c8312693c96f354d10a
Julien Buret <jburet@zeenea.com>
2022-01-10 19:24:55 +0100

    SSM already exists in used AMI

--- files/seed-amazon-linux.sh
+++ files/seed-amazon-linux.sh
@@ -13,10 +13,6 @@ sudo yum install git -y
 pip3 install -U boto3
 pip3 install -U ansible==4.9.0
 
-sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_arm64/amazon-ssm-agent.rpm
-sudo systemctl enable amazon-ssm-agent
-sudo systemctl start amazon-ssm-agent
-
 # Leaving a custom fact for future idempotency checking
 mkdir -p /etc/ansible/facts.d
 echo "{\"planted\": true}" > /etc/ansible/facts.d/seed.fact
